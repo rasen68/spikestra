c27042cbfa271a2c544ed70069b2d27be6c41d9c
rasen68 <chriszhao6868@gmail.com>
2026-08-02 15:10:54 -0400

    Refactor: private variables like a good ooper

--- neuron.py
+++ neuron.py
@@ -5,9 +5,9 @@ from __future__ import annotations
 class Neuron:
     ''' Stateful Neuron model with membrane and recovery '''
     c: tuple[int, int] # coordinate position
-    I: int # synaptic input
-    v: int # potential
-    u: int # recovery
+    _I: int # synaptic input
+    _v: int # potential
+    _u: int # recovery
 
     def __init__(self, coordinate: tuple[int, int]) -> Neuron:
         self.c = coordinate
@@ -16,21 +16,21 @@ class Neuron:
     def update(self):
         ''' computes u(t+1), v(t+1) based on equations 2.1 and 2.2       '''
         ''' call after self.I is loaded with input from Edges this is in '''
-        v, u = self.v, self.u # old values
-        self.v = u + self.I
-        if v == 1:
-            self.u = -5
-        else:
-            self.u = min(u + 1, 0)
-        self.I = 0 # reset 
+        v, u = self._v, self._u # old values
+        self._v = u + self.I
+        self._u = -5 if v == 1 else min(u + 1, 0)
+        self._I = 0 # reset 
 
     def spiked(self) -> bool:
-        return self.v >= 1
+        return self._v >= 1
+
+    def increment(self):
+        self._I += 1
     
     def reset(self):
-        self.I = 0
-        self.v = 0
-        self.u = 0
+        self._I = 0
+        self._v = 0
+        self._u = 0
 
 class Edge:
     ''' An Edge is a path between two neurons, from j to i '''
@@ -38,7 +38,7 @@ class Edge:
     i: Neuron
     j: Neuron
     D: int # axonal delay
-    d: int # delay counter
+    _d: int # delay counter
 
     def __init__(self, i: Neuron, j: Neuron, D: int) -> Edge:
         self.i = i
@@ -50,14 +50,11 @@ class Edge:
         # Update i.I based on equation 2.3
         # Together, the Edges containing i will create the sum
         # and i's update() reset i.I after each time step
-        if self.d == 1:
-            self.i.I += 1
+        if self._d == 1:
+            self.i.increment()
 
         # Update d based on j.v and equation 2.4
-        if self.j.spiked():
-            self.d = self.D
-        else:
-            self.d = max(self.d - 1, 0)
+        self._d = self.D if self.j.spiked() else max(self._d - 1, 0)
 
     def reset(self):
-        self.d = 0
+        self._d = 0
